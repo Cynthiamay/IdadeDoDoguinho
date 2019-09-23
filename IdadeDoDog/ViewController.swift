9b759d6d2c0c6c7d4dac590afeec44899d75f4d6
Cynthia <cynthiayamaoto@gmail.com>
2019-09-23 13:59:23 -0300

    versa2

--- IdadeDoDog/ViewController.swift
+++ IdadeDoDog/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tituloDoApp: UILabel!
+    
+    @IBOutlet weak var imagemDoDoguinho: UIImageView!
+    
+    @IBOutlet weak var campoDeTexto: UITextField!
+    
+    @IBOutlet weak var mostraIdade: UILabel!
 
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        
+        if let idadeHumana = Int(campoDeTexto.text!) {
+            let idadeDoguinho = idadeHumana * 7
+            
+            legendaResultado.text = "A Idade do Doguinho é:\(idadeDoguinho)"
+        }else{
+            legendaResultado.text = "Insira um número!"
+        }
+    
+        
+    }
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
-
-
 }
-
